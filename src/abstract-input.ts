4cf0dd0561ea33dae96f4198ead111539796b412
Sergey G <garinse@gmail.com>
2020-01-27 17:59:23 +0400

    common propertis and methods moved to abstract class

---
+++ src/abstract-input.ts
@@ -0,0 +1,82 @@
+import { EventEmmiter } from "./event-emmiter.js";
+
+export abstract class AbstractInput {
+  protected _parentElement: HTMLElement | null;
+  protected _controlElement: HTMLDivElement | null;
+  protected _hostInputElement: HTMLInputElement | null;
+  protected _value: number | null;
+  protected _text: string;
+  protected _isValid: boolean;
+  protected _timeout: number;
+
+  abstract _prefix: string;
+
+  valueChanged: EventEmmiter<number | null>;
+  textChanged: EventEmmiter<string>;
+  isValidChanged: EventEmmiter<boolean>;
+
+  constructor(parentElement: HTMLElement) {
+    this._parentElement = parentElement;
+    this._controlElement = null;
+    this._hostInputElement = null;
+    this._value = null;
+    this._text = '';
+    this._isValid = true;
+    this._timeout = 0;
+
+    this.valueChanged = new EventEmmiter();
+    this.textChanged = new EventEmmiter();
+    this.isValidChanged = new EventEmmiter();
+  }
+
+  protected render(): void {
+    if (!this._parentElement) {
+      return;
+    }
+    this._controlElement = document.createElement('div');
+    this._controlElement.className = `${this._prefix}control`;
+
+    this._hostInputElement = document.createElement('input');
+    this._hostInputElement.type = 'text';
+    this._hostInputElement.className = `${this._prefix}input`;
+    this._controlElement.append(this._hostInputElement);
+
+    this._parentElement.innerHTML = '';
+    this._parentElement.append(this._controlElement);
+  }
+
+  get hostElement(): HTMLElement | null {
+    return this._parentElement;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  get text(): string {
+    return this._text;
+  }
+
+  get isValid(): boolean {
+    return this._isValid;
+  }
+
+  protected _isValidUpdate() {
+    if (this._value === null && this._text === '' || !isNaN(parseFloat(String(this._value)))) {
+      this._isValid = true;
+      this._controlElement?.classList.remove('error');
+    } else {
+      this._isValid = false;
+      this._controlElement?.classList.add('error');
+    }
+
+    this.isValidChanged.emmit(this._isValid);
+  }
+
+  protected destroy() {
+    this._controlElement = null;
+    this._hostInputElement = null;
+    this._parentElement && (this._parentElement.innerHTML = '');
+    this._parentElement = null;
+  }
+}
--- src/numeric/numinput.ts
+++ src/numeric/numinput.ts
@@ -1,60 +1,21 @@
-import { EventEmmiter } from "../event-emmiter.js";
+import { AbstractInput } from "../abstract-input.js";
 
-export class NumInput implements EventListenerObject {
+export class NumInput extends AbstractInput implements EventListenerObject {
 
-  private _parentElement: HTMLElement | null;
-  private _controlElement: HTMLDivElement | null;
-  private _hostInputElement: HTMLInputElement | null;
-  private _value: number | null;
-  private _text: string;
-  private _isValid: boolean;
-  private _timeout: number;
-
-  valueChanged: EventEmmiter<number | null>;
-  textChanged: EventEmmiter<string>;
-  isValidChanged: EventEmmiter<boolean>;
+  _prefix: string;
 
   constructor(parentElement: HTMLElement) {
-    this._parentElement = parentElement;
-    this._controlElement = null;
-    this._hostInputElement = null;
-    this._value = null;
-    this._text = '';
-    this._isValid = true;
-    this._timeout = 0;
-
-    this.valueChanged = new EventEmmiter();
-    this.textChanged = new EventEmmiter();
-    this.isValidChanged = new EventEmmiter();
+    super(parentElement);
+    this._prefix = 'nm-';
   }
 
   render() {
-    if (!this._parentElement) {
-      return;
-    }
-    this._controlElement = document.createElement('div');
-    this._controlElement.className = 'nm-control';
-
-    this._hostInputElement = document.createElement('input');
-    this._hostInputElement.type = 'text';
-    this._hostInputElement.className = 'nm-input';
-    this._hostInputElement.addEventListener('input', this);
-    this._hostInputElement.addEventListener('change', this);
-    this._controlElement.append(this._hostInputElement);
-
-    this._parentElement.innerHTML = '';
-    this._parentElement.append(this._controlElement);
+    super.render();
+    this._hostInputElement?.addEventListener('input', this);
+    this._hostInputElement?.addEventListener('change', this);
   }
 
-  get hostElement(): HTMLElement | null {
-    return this._parentElement;
-  }
-
-  get value(): number | null | string {
-    return this._value;
-  }
-
-  set value(v: number | null | string) {
+  set value(v: any) {
     this._setValue(v);
 
     if (this._isValid) {
@@ -74,31 +35,11 @@ export class NumInput implements EventListenerObject {
     this._isValidUpdate();
   }
 
-  get text(): string {
-    return this._text;
-  }
-
   set text(v: string) {
     this._text = v;
     this._hostInputElement && (this._hostInputElement.value = this._text);
     this._hostInputElement?.dispatchEvent(new Event('change'));
     this.textChanged.emmit(this._text);
-  }
-
-  get isValid(): boolean {
-    return this._isValid;
-  }
-
-  private _isValidUpdate() {
-    if (this._value === null && this._text === '' || !isNaN(parseFloat(String(this._value)))) {
-      this._isValid = true;
-      this._controlElement?.classList.remove('error');
-    } else {
-      this._isValid = false;
-      this._controlElement?.classList.add('error');
-    }
-
-    this.isValidChanged.emmit(this._isValid);
   }
 
   handleEvent(event: Event): void {
@@ -114,14 +55,8 @@ export class NumInput implements EventListenerObject {
   }
 
   destroy() {
+    super.destroy();
     this._hostInputElement?.removeEventListener('input', this);
     this._hostInputElement?.removeEventListener('change', this);
-
-    this._controlElement = null;
-    this._hostInputElement = null;
-    if (this._parentElement) {
-      this._parentElement.innerHTML = '';
-    }
-    this._parentElement = null;
   }
 }
